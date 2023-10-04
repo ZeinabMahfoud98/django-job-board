1b24a464cfca30c63586c537fa80f2a197b81bd2
ASUS <asus@asus-X540LJ>
2023-10-04 17:32:50 -0400

    add model Job

--- job/models.py
+++ job/models.py
@@ -1,3 +1,23 @@
 from django.db import models
 
 # Create your models here.
+
+class Job(models.Model):
+    FULL_TIME="full time"
+    PART_TIME="part time"
+    JOB_TYPE=[
+        (FULL_TIME,"Full Time Work"),## first for DB second for human
+        (PART_TIME,"Part Time Work")
+    ]
+    title=models.CharField(max_length=100)
+
+    job_type=models.CharField(max_length=15,choices=JOB_TYPE)
+    description=models.TextField(max_length=1000)
+    published_at=models.DateTimeField(auto_now=True)
+    vacancy=models.IntegerField(default=1)
+    salary=models.IntegerField(default=0)
+    
+    experience=models.IntegerField(default=1)
+
+    def __str__(self) -> str:
+        return self.title
